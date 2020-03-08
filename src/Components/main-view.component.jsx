66eb29defbed79cd9ef155d5aee9363603d0caf3
Thabo James Maboya <james.maboya@gmail.com>
2020-03-08 14:08:14 +0200

    08-03-2020
    08-03-2020 - first attempt

---
+++ src/Components/main-view.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './main-view.styles.css'
+
+export const MainView = (props) => (
+    
+        <div className='main-container'>
+            <h1> {props.monsters.name}</h1>
+        </div>
+    
+)
